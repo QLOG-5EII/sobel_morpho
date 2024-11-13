77426710eced627c7d8fe42127e71670121e53ca
mplumejeau <maxime.plumejeau@gmail.com>
2024-11-13 10:44:28 +0100

    modif sobel filter

--- src/sobel.c
+++ src/sobel.c
@@ -15,6 +15,9 @@
 #include "sobel.h"
 
 void sobel(int width, int height, unsigned char *input, unsigned char *output) {
+  
+  #if 0
+
   int i, j;
 
   // Apply the filter
@@ -37,4 +40,30 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
     output[j * width] = 0;
     output[(j + 1) * width - 1] = 0;
   }
+
+  #endif
+
+  int i, j;
+
+  // Apply the filter
+  for (j = 1; j < height - 1; j++) {
+    for (i = 1; i < width - 1; i++) {
+      int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
+               input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
+               2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
+      int gy = -input[(j - 1) * width + i - 1] -
+               2 * input[(j - 1) * width + i] - input[(j - 1) * width + i + 1] +
+               input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
+               input[(j + 1) * width + i + 1];
+
+      output[(j-1)*width + i] = fminf(sqrtf(gx * gx + gy * gy)/3, 255);
+    }
+  }
+
+  // Fill the left and right sides
+  for (j = 0; j < height - 2; j++) {
+    output[j * width] = 0;
+    output[(j + 1) * width - 1] = 0;
+  }
+
 }
