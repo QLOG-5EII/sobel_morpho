f39123d972a2df305cfcbeb6bc28696ade7c73c1
Louenn Fradet <louenn.fradet@insa-rennes.fr>
2024-11-13 10:29:17 +0100

    Filtre moyenneur implemente dans sobel.c

--- src/sobel.c
+++ src/sobel.c
@@ -17,8 +17,8 @@
 void sobel(int width, int height, unsigned char *input, unsigned char *output) {
   int i, j;
 
-  // Apply the filter
-  for (j = 1; j < height - 1; j++) {
+  // Apply the filter THIS FILTER IS BAD
+  /*for (j = 1; j < height - 1; j++) {
     for (i = 1; i < width - 1; i++) {
       int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
                input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
@@ -29,6 +29,20 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
                input[(j + 1) * width + i + 1];
 
       output[(j-1)*width + i] = fminf(sqrtf(gx * gx + gy * gy), 255);
+    }
+  }*/
+  // My filter is BETTER
+  for (j = 1; j < height - 1; j++) {
+    for (i = 1; i < width - 1; i++) {
+      int sum = 0;
+      // Calculer la somme des pixels voisins
+      for (int y = -1; y <= 1; y++) {
+        for (int x = -1; x <= 1; x++) {
+          sum += input[(j + y) * width + (i + x)];
+        }
+      }
+      // Calculer la valeur moyenne
+      output[(j-1) * width + i] = sum / 9; // Division par 9 car on additionne 9 pixels
     }
   }
 
