fe906b16174afe895f18e7fe10d2924c05eed816
Valentin <valentin.faucheux02@gmail.com>
2024-11-13 11:27:33 +0100

    Ajout de la suppression du sample dans le script d'installation de hooks

--- install-hooks.sh
+++ install-hooks.sh
@@ -3,6 +3,7 @@
 # Script permettant de copier les hooks modifée dans le repo git 
 
 # A faire pour chaque hook modifié
+rm .git/hooks/pre-commit.sample
 cp hooks/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
 
