736c409ccf5239ef93400131ac7ebeb88aaa4fe1
RAHLIOussama <rahli.oussama2015@gmail.com>
2024-11-18 08:57:36 +0100

    Filtre Moyenneur

--- src/sobel.c
+++ src/sobel.c
@@ -18,23 +18,26 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
   int i, j;
 
   // Apply the filter
-  for (j = 1; j < height - 1; j++) {
+ for (j = 1; j < height - 1; j++) {
     for (i = 1; i < width - 1; i++) {
-      int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
-               input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
-               2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
-      int gy = -input[(j - 1) * width + i - 1] -
-               2 * input[(j - 1) * width + i] - input[(j - 1) * width + i + 1] +
-               input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
-               input[(j + 1) * width + i + 1];
+      int sum = input[(j - 1) * width + i - 1] + input[(j - 1) * width + i] +
+                input[(j - 1) * width + i + 1] + input[j * width + i - 1] +
+                input[j * width + i] + input[j * width + i + 1] +
+                input[(j + 1) * width + i - 1] + input[(j + 1) * width + i] +
+                input[(j + 1) * width + i + 1];
 
-      output[(j - 1) * width + i] = (gx * gx + gy * gy) / 8; // TODO
+      output[j * width + i] = sum / 9; // Moyenne des pixels
     }
   }
 
   // Fill the left and right sides
-  for (j = 0; j < height - 2; j++) {
+  for (j = 0; j < height; j++) {
     output[j * width] = 0;
-    output[(j + 1) * width - 1] = 0;
+    output[j * width + width - 1] = 0;
+  }
+
+  for (i = 0; i < width; i++) {
+    output[i] = 0;
+    output[(height - 1) * width + i] = 0;
   }
 }
