5f99ff6940bd86c9d9d45608ca38b9cef71ae46a
eii <kexin.chen@insa-rennes.fr>
2024-11-18 08:17:30 +0100

    Activate Sobel function to test CI

--- src/sobel.c
+++ src/sobel.c
@@ -22,7 +22,7 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
   for (j = 1; j < height - 1; j++) {
     for (i = 1; i < width - 1; i++) {
       //Sobel
-        /*
+
       int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
                input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
                2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
@@ -32,14 +32,15 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
                input[(j + 1) * width + i + 1];
 
       output[(j - 1) * width + i] = (gx * gx + gy * gy) / 8;
-      */
+
 
       //moyenneur
+/*
       output[(j - 1) * width + i] =
           input[(j - 1) * (width-1)] + input[(j - 1) * width + i - 1] + input[(j + 1) * width + i - 1] +
           input[(j - 1) * width + i + 1] + input[j * width + i + 1] + input[(j + 1) * width + i + 1] +
           input[(j - 1) * width + i + 1] + input[j * width + i + 1] + input[(j + 1) * width + i + 1];
-
+*/
    }
   }
 
