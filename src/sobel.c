f246fb7876576f8a8eb37a7e25c3ac3f492a8fa9
Mouad FARAH <Mouad.Farah@insa-rennes.fr>
2025-10-19 16:49:02 +0200

    Remplacement du filtre sobel par filtre moyenneur

--- src/sobel.c
+++ src/sobel.c
@@ -19,22 +19,25 @@ void sobel(int width, int height, unsigned char *input, unsigned char *output) {
 
   // Apply the filter
   for (j = 1; j < height - 1; j++) {
-    for (i = 1; i < width - 1; i++) {
-      int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
-               input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
-               2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
-      int gy = -input[(j - 1) * width + i - 1] -
-               2 * input[(j - 1) * width + i] - input[(j - 1) * width + i + 1] +
-               input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
-               input[(j + 1) * width + i + 1];
-
-      output[(j-1)*width + i] = fminf(sqrtf(gx * gx + gy * gy), 255);
+        for (i = 1; i < width - 1; i++) {
+            int sum = 0;
+            for (int y = -1; y <= 1; y++) {
+                for (int x = -1; x <= 1; x++) {
+                    sum += input[(j + y) * width + (i + x)];
+                }
+            }
+            output[j * width + i] = sum / 9; // moyenne
+        }
     }
-  }
-
+    
   // Fill the left and right sides
   for (j = 0; j < height - 2; j++) {
     output[j * width] = 0;
     output[(j + 1) * width - 1] = 0;
   }
+  
+  for (i = 0; i < width; i++) {
+        output[i] = 0;
+        output[(height - 1) * width + i] = 0;
+    }
 }
