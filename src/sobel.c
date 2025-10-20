c001053836499f6b7ba0c91214cf0a0c86700369
sachatms <sachathomas56.st@gmail.com>
2025-10-20 11:33:30 +0200

    feat: Replace Sobel edge detection with mean averaging filter
    Replace the Sobel gradient calculation with a 3x3 mean/average filter
    that smooths the image by averaging neighboring pixel values. This
    provides a simple blur effect instead of edge detection.
    
    - Sum all 9 pixels in 3x3 neighborhood
    - Divide by 9 to get average
    - Maintains same function signature for compatibility

--- src/sobel.c
+++ src/sobel.c
@@ -8,33 +8,37 @@
         ============================================================================
 */
 
+#include "sobel.h"
+
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "sobel.h"
+void sobel(int width, int height, unsigned char* input, unsigned char* output) {
+    int i, j;
 
-void sobel(int width, int height, unsigned char *input, unsigned char *output) {
-  int i, j;
+    // Apply mean/average filter (3x3 box blur)
+    for (j = 1; j < height - 1; j++) {
+        for (i = 1; i < width - 1; i++) {
+            // Sum all 9 pixels in the 3x3 neighborhood
+            int sum = input[(j - 1) * width + (i - 1)] +  // top-left
+                      input[(j - 1) * width + i] +        // top-center
+                      input[(j - 1) * width + (i + 1)] +  // top-right
+                      input[j * width + (i - 1)] +        // middle-left
+                      input[j * width + i] +              // center
+                      input[j * width + (i + 1)] +        // middle-right
+                      input[(j + 1) * width + (i - 1)] +  // bottom-left
+                      input[(j + 1) * width + i] +        // bottom-center
+                      input[(j + 1) * width + (i + 1)];   // bottom-right
 
-  // Apply the filter
-  for (j = 1; j < height - 1; j++) {
-    for (i = 1; i < width - 1; i++) {
-      int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
-               input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
-               2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
-      int gy = -input[(j - 1) * width + i - 1] -
-               2 * input[(j - 1) * width + i] - input[(j - 1) * width + i + 1] +
-               input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
-               input[(j + 1) * width + i + 1];
-
-      output[(j-1)*width + i] = fminf(sqrtf(gx * gx + gy * gy), 255);
+            // Average by dividing by 9
+            output[(j - 1) * width + i] = sum / 9;
+        }
     }
-  }
 
-  // Fill the left and right sides
-  for (j = 0; j < height - 2; j++) {
-    output[j * width] = 0;
-    output[(j + 1) * width - 1] = 0;
-  }
+    // Fill the left and right sides
+    for (j = 0; j < height - 2; j++) {
+        output[j * width] = 0;
+        output[(j + 1) * width - 1] = 0;
+    }
 }
