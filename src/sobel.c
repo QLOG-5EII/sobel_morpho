8f0647f5f74e7d325255d66b141701e9bb2961c1
Soufiane Lotfi <soufianelotfi-lab@gmail.com>
2025-10-20 11:10:42 +0200

    Remplacement du filtre Sobel par un filtre médian 3x3

--- src/sobel.c
+++ src/sobel.c
@@ -14,27 +14,48 @@
 
 #include "sobel.h"
 
+#include <stdlib.h>
+
 void sobel(int width, int height, unsigned char *input, unsigned char *output) {
-  int i, j;
+    int i, j;
 
-  // Apply the filter
-  for (j = 1; j < height - 1; j++) {
-    for (i = 1; i < width - 1; i++) {
-      int gx = -input[(j - 1) * width + i + 1] - 2 * input[j * width + i - 1] -
-               input[(j + 1) * width + i - 1] + input[(j - 1) * width + i + 1] +
-               2 * input[j * width + i + 1] + input[(j + 1) * width + i + 1];
-      int gy = -input[(j - 1) * width + i - 1] -
-               2 * input[(j - 1) * width + i] - input[(j - 1) * width + i + 1] +
-               input[(j + 1) * width + i - 1] + 2 * input[(j + 1) * width + i] +
-               input[(j + 1) * width + i + 1];
+    // Parcours de l'image sauf les bords
+    for (j = 1; j < height - 1; j++) {
+        for (i = 1; i < width - 1; i++) {
+            unsigned char voisin[9];
+            int k = 0;
 
-      output[(j - 1) * width + i] = (gx * gx + gy * gy) / 8; // TODO
+            // Récupérer les 9 pixels voisins (fenêtre 3x3)
+            for (int y = -1; y <= 1; y++) {
+                for (int x = -1; x <= 1; x++) {
+                    voisin[k++] = input[(j + y) * width + (i + x)];
+                }
+            }
+
+            // Trier les 9 valeurs pour trouver la médiane
+            for (int a = 0; a < 9; a++) {
+                for (int b = a + 1; b < 9; b++) {
+                    if (voisin[a] > voisin[b]) {
+                        unsigned char temp = voisin[a];
+                        voisin[a] = voisin[b];
+                        voisin[b] = temp;
+                    }
+                }
+            }
+
+            // La médiane est la 5e valeur après tri
+            output[j * width + i] = voisin[4];
+        }
     }
-  }
 
-  // Fill the left and right sides
-  for (j = 0; j < height - 2; j++) {
-    output[j * width] = 0;
-    output[(j + 1) * width - 1] = 0;
-  }
+    // Mettre les bords à 0 (noir)
+    for (i = 0; i < width; i++) {
+        output[i] = 0;
+        output[(height - 1) * width + i] = 0;
+    }
+    for (j = 0; j < height; j++) {
+        output[j * width] = 0;
+        output[j * width + (width - 1)] = 0;
+    }
 }
+
